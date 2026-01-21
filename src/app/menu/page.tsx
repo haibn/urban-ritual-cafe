da263ad1f1a71e92ccf5df9ab542d18ee19d6220
haibn <haibinyuy@berkeley.edu>
2026-01-20 18:18:28 -0800

    created file structure and added fonts

---
+++ src/app/menu/page.tsx
@@ -0,0 +1,9 @@
+export default function Menu() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between bg-white px-16 py-32 sm:items-start dark:bg-black">
+        Menu Page
+      </main>
+    </div>
+  );
+}
